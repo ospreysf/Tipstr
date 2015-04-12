9a1545a5cb1df4ec843a3d8d5ad1cc78408ca5cc
ospreysf <arthur.law@gmail.com>
2015-04-11 18:22:14 -0700

    Initial Commit

---
+++ TipstrTests/TipstrTests.swift
@@ -0,0 +1,36 @@
+//
+//  TipstrTests.swift
+//  TipstrTests
+//
+//  Created by Arthur Law on 4/11/15.
+//  Copyright (c) 2015 Arthur Law. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TipstrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
