99acab0baf474291e0623485e9a69f4c40b50baa
ospreysf <arthur.law@gmail.com>
2015-04-17 07:35:53 -0700

    Added Walkthrough Animation

--- Tipstr/ViewController.swift
+++ Tipstr/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipAllPercentages = [0.18, 0.2, 0.22]
+        var tipPercentages = tipAllPercentages[tipControl.selectedSegmentIndex]
+        
+        //println("user edited bill amount")
+        var billAmount = NSString(string: billField.text).doubleValue
+        //println("bill amount is now \(billAmount)")
+        
+        var tip = billAmount * tipPercentages
+        //println ("the tip is now \(tip)")
+        tipLabel.text = "$\(tip)"
+        
+        var total = billAmount + tip
+        //println("the total is now \(total)")
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+
+    
 
 }
 
